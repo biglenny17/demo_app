febd45e3c355a09c3a01b8a99829deee128e1071
lenny <lenny.worsono@meltwater.org>
2012-04-03 22:17:52 +0000

    Initial commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '69419c4f41e9d82f15ad9573f1c0085a7a7c0c62945f59ea7a32ebb4866cd529ba1b99e4132c20200139984359529edac683c26df3f06df5e7cdc3ffb87cfabc'
